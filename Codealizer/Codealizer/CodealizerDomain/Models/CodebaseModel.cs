2bef98b0c9bbe366a6bb0c41ccb05f88e4c035e0
Nimrod Gaudin <nimrod.gaudin@wheelright.co.uk>
2025-12-29 12:15:15 +0000

    Add Models - CodebaseModel & ClassModel & MethodModel

---
+++ Codealizer/Codealizer/CodealizerDomain/Models/CodebaseModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodealizerDomain.Models;
+
+public record CodebaseModel
+{
+    public List<ClassModel> Classes { get; init; } = new();
+}
---
+++ Codealizer/Codealizer/CodealizerDomain/Models/MethodModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodealizerDomain.Models;
+
+public record MethodModel
+{
+    public Guid MethodId { get; init; }
+    public string MethodName { get; init; } = string.Empty;
+    public List<(Guid, string)> CalledMethods { get; init; } = new(); // list of (MethodId, Parameters as one string) that this method calls
+}
